f7a2f73170c8350541b6ee60044dd088c3a75842
scar0125 <ryoga.yoshisato@gmail.com>
2021-07-31 16:40:39 +0900

    add detail display

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,4 +12,9 @@ class PostController extends Controller
         return view('index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
+    }
+    
 }
