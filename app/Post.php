87e51db5f60d33fb59df9223b6867cdc20e27dc0
scar0125 <ryoga.yoshisato@gmail.com>
2021-07-31 21:06:07 +0900

    add create

--- app/Post.php
+++ app/Post.php
@@ -5,7 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
+{   
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
     public function getPaginateByLimit(int $limit_count = 5)
     {
     // updated_atで降順に並べたあと、limitで件数制限をかける
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,11 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='create'>[<a href='/posts/create'>create</a>]</p>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <a href='/posts/{{ $post->id }}'><h2 class='title'>{{ $post->title }}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
